7ed7a018cb6be559ba7e3401a9a0fc2d6b63b081
lucablackwell <luca@lucablackwell.com>
2022-06-20 09:52:58 +0100

    add heroica.php

---
+++ heroica.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Progress
+ *  Linear progress - option for length
+ *  Randomize chests, potions, enemies (entities)
+ *  Difficulty increases puzzle difficulty, harder enemy count & potion chances
+ *  Display progress with entities
+ *    [----*----E---p---E----]
+ *  No point in doors with keys because linear
+ *    Doors with puzzles instead?
+ *      Guess the number / mastermind
+ */
