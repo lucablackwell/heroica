e9a78df1a58fab4cb53dfb0be76fbb5f99428e26
lucablackwell <luca@lucablackwell.com>
2022-06-20 09:54:30 +0100

    dice-based

--- heroica.php
+++ heroica.php
@@ -10,3 +10,40 @@
  *    Doors with puzzles instead?
  *      Guess the number / mastermind
  */
+
+/* Dice Movement
+ *  6 sides
+ *  Pick up potions and carry on
+ *  Stop for enemies and chests
+ *  Shield
+ *    Move up to 4 spaces / use Ranged skill
+ *  Sword / 3
+ *    Move 3 spaces
+ *  Skull / 2
+ *    Move 2 spaces
+ *  Sword & Skull / 1
+ *    Move 1 spaces
+ */
+
+/* Dice Fighting
+ *  6 sides
+ *  Shield
+ *    Defeat / use Melee skill
+ *  Sword / 3
+ *    Defeat
+ *  Skull / 2
+ *    Lose health equal to strength + (move back one space)
+ *  Sword & Skull / 1
+ *    Defeat and lose health equal to strength + (move back one space)
+ */
+
+/* Chests
+ *  Shield
+ *    Find 2 gold
+ *  Sword / 3
+ *    Find 1 gold
+ *  Skull / 2
+ *    Lose 1 health
+ *  Sword & Skull / 1
+ *    Find 1 gold and lose 1 health
+ */
