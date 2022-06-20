874a655052878ac7188be4b4600b3200b2cf681e
lucablackwell <luca@lucablackwell.com>
2022-06-20 09:54:58 +0100

    potions, skills, monsters, heroes

--- heroica.php
+++ heroica.php
@@ -47,3 +47,51 @@
  *  Sword & Skull / 1
  *    Find 1 gold and lose 1 health
  */
+
+/* Potions
+ *  Usable at any time
+ *  Each only usable once
+ *  Life
+ *    Restore 2 health
+ *  Luck
+ *    Re-roll the dice
+ *  Speed
+ *    Move up to 4 extra spaces
+ *  Strength
+ *    Defeat an adjacent monster
+ */
+
+/* Heroes / Skills
+ *  Barbarian
+ *    Melee: defeat all adjacent monsters + move 1 space
+ *  Wizard
+ *    Ranged: defeat a monster up to 4 spaces away even around corners
+ *  Druid
+ *    Ranged: restore full health
+ *  Knight
+ *    Ranged: move up to 2 spaces and defeat an adjacent monster
+ */
+
+/* Monsters
+ *  Goblin Warrior
+ *    1 Strength
+ *  Goblin Guardian
+ *    2 Strength
+ *  Goblin King
+ *    3 Strength
+ */
+
+/* Weapons - cost 3, sell back for 2
+ *  Axe
+ *    Melee: defeat all adjacent monsters
+ *  Wand
+ *    Ranged: defeat a monster up to 3 spaces away
+ *  Staff
+ *    Ranged: restore up to 2 health
+ *  Bow
+ *    Ranged: defeat all monsters up to 5 spaces away (costs 1 health)
+ *  Dagger
+ *    Melee: defeat the monster and take 1 gold from the store
+ *  Sword
+ *    Melee: defeat the monster and take 2 gold from the store (costs 1 health)
+ */
